4d0184ddfe9a1bedb15d242d5dadfc72f3aeb9ae
Davide <99175717+dbf01@users.noreply.github.com>
2022-02-09 21:15:07 +0100

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,9 @@
 #this should be file 1, right?
+'''
+I am using visual studio Code
+'''
+import pandas as pd
+
+df_disease = pd.read_csv("disease_evidences.tsv", "\t")
+
+df_gene = pd.read_csv("gene_evidences.tsv", "\t")
