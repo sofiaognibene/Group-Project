53d8eae014b5cb66d41147e4ca0670b616975966
sofiaognibene <sofia.ognibene@studio.unibo.it>
2022-02-10 16:57:53 +0100

    Questions 8 and 9

--- part2.py
+++ part2.py
@@ -1,6 +1,7 @@
 import csv
 from turtle import pd
 import pandas as pd
+import numpy
 
 #1
 df_disease = pd.read_csv("disease_evidences.tsv", "\t")
@@ -26,11 +27,38 @@ print(g_sentence_list)'''
 df_duplicates_disease = df_disease.loc[:, ["diseaseid", "disease_name"]].drop_duplicates().sort_values
 
 #6
-
-d = "diseaseid == " + "'" + input("diseaseid: ") + "'"
+'''d = "diseaseid == " + "'" + input("diseaseid: ") + "'"
 d_sentences = df_disease.query(d)["sentence"]
 d_sentence_list = []
 
 for e in d_sentences:
     d_sentence_list.append(e)
-print(d_sentence_list)
+print(d_sentence_list)'''
+
+#8
+'''gene = "geneid ==" + input("geneid: ")
+query_gene = df_gene.query(gene)["pmid"]
+
+gene_disease = []
+
+for a in query_gene:
+    query_disease = "pmid ==" + str(a)
+    query = df_disease.query(query_disease).loc[:, ["diseaseid", "disease_name"]].drop_duplicates()
+    gene_disease.append(query)
+
+gd_final = pd.concat(gene_disease)
+print(gd_final)'''
+
+#9
+disease = "diseaseid ==" + "'" + input("diseaseid: ") + "'"
+query_disease = df_disease.query(disease)["pmid"]
+
+disease_gene = []
+
+for z in query_disease:
+    query_gene = "pmid ==" + str(z)
+    g_query = df_gene.query(query_gene).loc[:, ["geneid"]].drop_duplicates()
+    disease_gene.append(g_query)
+
+dg_final = pd.concat(disease_gene)
+print(dg_final)
